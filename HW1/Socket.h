ad947ce66f9f21a60a21899d7a793c2db4a8b5db
Jim Counts <count009@cougars.csusm.edu>
2013-02-28 04:29:24 -0800

    Moved Socket to file

---
+++ HW1/Socket.h
@@ -0,0 +1,62 @@
+#ifndef SOCKET_H
+#define SOCKET_H
+
+using namespace std;
+
+class Socket
+{
+private:
+    struct sockaddr_in addr;
+
+    int socket_fd;
+
+protected:
+    void acquire_socket()
+    {
+        socket_fd = socket( AF_INET, SOCK_STREAM, 0 );
+    }
+
+    void parse_address( string address_spec )
+    {
+        unsigned found = address_spec.find_first_of( ":" );
+
+        if ( found != string::npos )
+        {
+            address_spec[found] = ' ';
+        }
+
+        stringstream buffer( address_spec );
+        string a[2];
+
+        for ( int i = 0; i < 2; ++i )
+        {
+            buffer >> a[i];
+        }
+
+        bzero( &addr, sizeof( addr ) );
+        addr.sin_family = AF_INET;
+        addr.sin_addr.s_addr = inet_addr( a[0].c_str() );
+        addr.sin_port = htons( atoi( a[1].c_str() ) );
+    }
+
+public:
+    Socket() : socket_fd( -1 ) {}
+
+    ~Socket()
+    {
+        close( socket_fd );
+    }
+
+    struct sockaddr_in get_address()
+    {
+        return addr;
+    }
+
+    int get_descriptor()
+    {
+        return socket_fd;
+    }
+
+};
+
+#endif
