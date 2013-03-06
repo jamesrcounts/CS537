0306160f56500615db2ff70854632817b291ae2e
Jim Counts <count009@cougars.csusm.edu>
2013-03-06 15:08:17 -0800

    Improved client

--- HW1/httpClient.cpp
+++ HW1/httpClient.cpp
@@ -4,89 +4,143 @@
 #include <strings.h>
 #include <iostream>
 #include <sys/types.h>
+#include <sys/time.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-#define MAXLINE 256
+#define MAXLINE 32768
 
 using namespace std;
 
-void http_client( FILE *fp, int socket_fd );
+float version = 1.0f;
+int master_socket = -1;
 
-int main( int argc, char *argv[] )
+int acquire_socket( in_addr_t s_addr, uint16_t sin_port )
 {
-
-    int	socket_fd;
-    struct  sockaddr_in servaddr;
-
-    if ( argc != 3 )
+    if ( master_socket == -1 )
     {
-        printf( "Usage: httpClient <address> <port> \n" );
-        return -1;
-    }
+        int socket_fd = socket( AF_INET, SOCK_STREAM, 0 );
 
-    socket_fd = socket( AF_INET, SOCK_STREAM, 0 );
-
-    if ( socket_fd < 0 )
-    {
-        fprintf( stderr, "Error creating socket, errno = %d (%s) \n",
-                 errno, strerror( errno ) );
-        return -1;
-    }
-
-    bzero( &servaddr, sizeof( servaddr ) );
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = inet_addr( argv[1] );
-    servaddr.sin_port = htons( atoi( argv[2] ) );
-
-    if ( connect( socket_fd, ( struct sockaddr * )&servaddr, sizeof( servaddr ) ) == -1 )
-    {
-        fprintf( stderr, "Error unable to connect to socket, errno = %d (%s) \n", errno,
-                 strerror( errno ) );
-        return -1;
-    }
-
-    http_client( stdin, socket_fd );
-
-    return 0;
-
-}
-
-void http_client( FILE *fp, int socket_fd )
-{
-    int buffer_size = 1024;
-    char buffer[buffer_size];
-
-    string request( "HEAD /test.html HTTP/1.0\n\n" );
-
-    write( socket_fd, ( void * )request.c_str(), strlen( request.c_str() ) );
-
-    int last_read = 0;
-
-    string result;
-
-    do
-    {
-        bzero( buffer, buffer_size );
-        last_read = read(
-                        socket_fd,
-                        ( void * )buffer,
-                        buffer_size );
-
-        if ( last_read == -1 )
+        if ( socket_fd < 0 )
         {
-            break;
+            fprintf( stderr,
+                     "Error creating socket, errno = %d (%s) \n",
+                     errno,
+                     strerror( errno ) );
+            return -1;
         }
 
+        struct  sockaddr_in servaddr;
 
-        result += buffer;
+        bzero( &servaddr, sizeof( servaddr ) );
+
+        servaddr.sin_family = AF_INET;
+
+        servaddr.sin_addr.s_addr = s_addr;
+
+        servaddr.sin_port = sin_port;
+
+        int connect_result =  connect( socket_fd,
+                                       ( struct sockaddr * )&servaddr,
+                                       sizeof( servaddr ) );
+
+        if ( connect_result == -1 )
+        {
+            fprintf( stderr,
+                     "Error unable to connect to socket, errno = %d (%s) \n",
+                     errno,
+                     strerror( errno ) );
+            return -1;
+        }
+
+        master_socket = socket_fd;
     }
-    while ( 0 < last_read );
 
-    cout << result << endl;
+    return master_socket;
 }
 
+void release_socket()
+{
+    if ( version == 1.0f )
+    {
+        close( master_socket );
+        master_socket = -1;
+    }
+}
 
+void send_request( int socket_fd )
+{
+    string request( "GET /test.html HTTP/1.0\n\n" );
+    write( socket_fd, request.c_str(), request.size() );
+}
+
+string get_response( int socket_fd )
+{
+    int buffer_size = MAXLINE;
+    char buffer[MAXLINE + 1];
+    bzero( buffer, buffer_size );
+    int last_read = read( socket_fd, buffer, buffer_size );
+
+    if ( -1 == last_read )
+    {
+        fprintf( stderr,
+                 "Error unable to read socket, errno = %d (%s) \n",
+                 errno,
+                 strerror( errno ) );
+        exit( -1 );
+    }
+
+    return string( buffer );
+}
+
+int main( int argc, char const *argv[] )
+{
+    if ( argc < 3 )
+    {
+        printf( "Usage: httpClient <address> <port> [version]\n" );
+        return -1;
+    }
+
+    if ( argc == 4 )
+    {
+        version = strtof( argv[3], NULL );
+        cout << "Using HTTP " << version << " as requested..." << endl;
+    }
+
+
+    int i = 0;
+    struct timeval startat, endat;
+    gettimeofday( &startat, NULL );
+
+    for ( ; i < ( 1000 ); ++i )
+    {
+        int socket_fd = acquire_socket(
+                            inet_addr( argv[1] ),
+                            htons( atoi( argv[2] ) ) );
+
+        send_request( socket_fd );
+        get_response( socket_fd );
+
+        release_socket();
+    }
+
+    gettimeofday( &endat, NULL );
+
+    int sec = endat.tv_sec - startat.tv_sec;
+    int usec = endat.tv_usec - startat.tv_usec;
+
+    if ( usec < 0 )
+    {
+        --sec;
+        usec += 1000000;
+    }
+
+    cout << i << " requests in: "
+         << sec << " seconds and "
+         << usec << " microseconds." << endl;
+
+    return 0;
+}
