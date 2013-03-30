0b621e46dc27504f73a78dc98c4a96f55f994472
Jim Counts <count009@cougars.csusm.edu>
2013-03-29 19:56:36 -0700

    Converted test servers touse InternetSockets

--- HW2/test.cpp
+++ HW2/test.cpp
@@ -7,9 +7,8 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <unistd.h>
-// #include <sys/types.h>
 #include "TcpSocket.h"
-// #include "SocketConnection.h"
+#include "TcpListeningSocket.h"
 
 using namespace igloo;
 
@@ -125,197 +124,19 @@ Context( DescribeASocketConnection )
 
 void *caseServer( void *state )
 {
-    struct addrinfo hints;
-    memset( &hints, 0, sizeof hints );
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE; // use my IP
-
-    int rv;
-    struct addrinfo  *servinfo;
-
-    if ( ( rv = getaddrinfo( NULL, "3490", &hints, &servinfo ) ) != 0 )
-    {
-        fprintf( stderr, "getaddrinfo: %s\n", gai_strerror( rv ) );
-        return NULL;
-    }
-
-    // loop through all the results and bind to the first we can
-    struct addrinfo *p;
-
-    int sockfd;  // listen on sock_fd, new connection on new_fd
-
-    for ( p = servinfo; p != NULL; p = p->ai_next )
-    {
-        if ( ( sockfd = socket( p->ai_family,
-                                p->ai_socktype,
-                                p->ai_protocol ) ) == -1 )
-        {
-            perror( "server: socket" );
-            continue;
-        }
-
-        int yes = 1;
-
-        if ( setsockopt( sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-                         sizeof( int ) ) == -1 )
-        {
-            perror( "setsockopt" );
-            exit( 1 );
-        }
-
-        if ( bind( sockfd, p->ai_addr, p->ai_addrlen ) == -1 )
-        {
-            close( sockfd );
-            perror( "server: bind" );
-            continue;
-        }
-
-        break;
-    }
-
-    if ( p == NULL )
-    {
-        fprintf( stderr, "server: failed to bind\n" );
-        return NULL;
-    }
-
-    freeaddrinfo( servinfo ); // all done with this structure
-
-    if ( listen( sockfd, 4 ) == -1 )
-    {
-        perror( "listen" );
-        exit( 1 );
-    }
-
-    for ( int i = 0; i < 10; ++i )
-    {
-        struct sockaddr_storage their_addr; // connector's address information
-        socklen_t sin_size = sizeof their_addr;
-        int new_fd = accept( sockfd,
-                             ( struct sockaddr * )&their_addr,
-                             &sin_size );
-
-        if ( new_fd == -1 )
-        {
-            perror( "accept" );
-            return NULL;
-        }
-
-        char str[1024];
-        bzero( str, 1023 );
-
-        if ( read( new_fd, str, 1023 )  == 0 )
-        {
-            close( new_fd );
-            return NULL;
-        }
-
-        for ( unsigned int i = 0; i < strlen( str ); i++ )
-        {
-            if ( str[i] >= 97 && str[i] <= 122 )
-            {
-                str[i] = str[i] - 32;
-            }
-        }
-
-        write( new_fd, str, strlen( str ) );
-
-        close( new_fd );
-    }
-
-    close( sockfd );
+    TcpListeningSocket t( 4, "127.0.0.1", 3490 );
+    SocketConnection s = t.acceptConnection();
+    string str = s.readMessage();
+    transform( str.begin(), str.end(), str.begin(), ::toupper );
+    s.sendMessage( str );
     return NULL;
 }
 
 void *pong( void *state )
 {
-    struct addrinfo hints;
-    memset( &hints, 0, sizeof hints );
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE; // use my IP
-
-    int rv;
-    struct addrinfo  *servinfo;
-
-    if ( ( rv = getaddrinfo( NULL, "3490", &hints, &servinfo ) ) != 0 )
-    {
-        fprintf( stderr, "getaddrinfo: %s\n", gai_strerror( rv ) );
-        return NULL;
-    }
-
-    // loop through all the results and bind to the first we can
-    struct addrinfo *p;
-
-    int sockfd;  // listen on sock_fd, new connection on new_fd
-
-    for ( p = servinfo; p != NULL; p = p->ai_next )
-    {
-        if ( ( sockfd = socket( p->ai_family,
-                                p->ai_socktype,
-                                p->ai_protocol ) ) == -1 )
-        {
-            perror( "server: socket" );
-            continue;
-        }
-
-        int yes = 1;
-
-        if ( setsockopt( sockfd, SOL_SOCKET, SO_REUSEADDR, &yes,
-                         sizeof( int ) ) == -1 )
-        {
-            perror( "setsockopt" );
-            exit( 1 );
-        }
-
-        if ( bind( sockfd, p->ai_addr, p->ai_addrlen ) == -1 )
-        {
-            close( sockfd );
-            perror( "server: bind" );
-            continue;
-        }
-
-        break;
-    }
-
-    if ( p == NULL )
-    {
-        fprintf( stderr, "server: failed to bind\n" );
-        return NULL;
-    }
-
-    freeaddrinfo( servinfo ); // all done with this structure
-
-    if ( listen( sockfd, 4 ) == -1 )
-    {
-        perror( "listen" );
-        exit( 1 );
-    }
-
-    struct sockaddr_storage their_addr; // connector's address information
-
-    socklen_t sin_size = sizeof their_addr;
-
-    int new_fd = accept( sockfd,
-                         ( struct sockaddr * )&their_addr,
-                         &sin_size );
-
-    if ( new_fd == -1 )
-    {
-        perror( "accept" );
-        return NULL;
-    }
-
-    close( sockfd ); // child doesn't need the listener
-
-    if ( send( new_fd, "Hello, world!", 13, 0 ) == -1 )
-    {
-        perror( "send" );
-    }
-
-    close( new_fd );
-
+    TcpListeningSocket t( 4, "127.0.0.1", 3490 );
+    SocketConnection s = t.acceptConnection();
+    s.sendMessage( "Hello, world!" );
     return NULL;
 }
 
